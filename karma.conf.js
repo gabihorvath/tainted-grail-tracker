1a6e2ef459c133730ab5f3032c18476f7bc1b35b
Rayina <rayina@it-zuhause.de>
2022-04-18 12:46:25 +0200

    Deploy to github pages

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/grail-tracker'),
+      dir: require('path').join(__dirname, './coverage/tainted-grail-tracker'),
       subdir: '.',
       reporters: [
         { type: 'html' },
